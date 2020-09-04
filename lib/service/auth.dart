d73f33747e5a451e54d1753c8ca90c54543e51d8
Canh Huy <huycanh14@gmail.com>
2020-09-04 14:32:32 +0700

    Adding Google Sign-In to the Auth class

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -1,29 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Account {
   final String uid;
+
   Account({@required this.uid});
 }
 
 abstract class AuthBase {
   Stream<Account> get onAuthStateChange;
+
   Future<Account> currentUser();
+
   Future<Account> signInAnonymously();
+
   Future<void> signOut();
+
+  Future<Account> signInWithGoogle();
 }
 
 class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
-  Account _userFromFirebase(User user){
-    if(user == null)
-      return null;
+
+  Account _userFromFirebase(User user) {
+    if (user == null) return null;
     return Account(uid: user.uid);
   }
 
   @override
-  Stream<Account> get onAuthStateChange{
-    return _firebaseAuth.authStateChanges().asyncMap((firebaseUser) => _userFromFirebase(firebaseUser));
+  Stream<Account> get onAuthStateChange {
+    return _firebaseAuth
+        .authStateChanges()
+        .asyncMap((firebaseUser) => _userFromFirebase(firebaseUser));
   }
 
   @override
@@ -36,6 +46,29 @@ class Auth implements AuthBase {
   Future<Account> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
     return _userFromFirebase(authResult.user);
+  }
+
+  @override
+  Future<Account> signInWithGoogle() async {
+    final googleSignIn = new GoogleSignIn();
+    final googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final googleAuth = await googleAccount.authentication;
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        final authResult = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+              idToken: googleAuth.idToken, accessToken: googleAuth.accessToken),
+        );
+        return _userFromFirebase(authResult.user);
+      } else {
+        throw PlatformException(
+            code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
+            message: 'Missing Google Auth Token');
+      }
+    } else {
+      throw PlatformException(
+          code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
+    }
   }
 
   @override
